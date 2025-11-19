1db42691a7211b2592b2f094551592e076a2f3b6
Alif <Alif@aDellVanMijnVader>
2025-11-19 15:56:33 +0700

    For Deployment

--- src/main/java/com/proyekquiz/util/DButil.java
+++ src/main/java/com/proyekquiz/util/DButil.java
@@ -5,9 +5,11 @@ import java.sql.SQLException;
 
 public class DButil{
 
-	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/webproquiz2";
-	private static final String JDBC_USER = "root"; 
-	private static final String JDBC_PASSWORD = ""; 
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/webproquiz2";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "";
+
+
 
 
     static{
